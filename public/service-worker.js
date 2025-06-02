14e153766401713761ec6ba8ddf71d0551652b0d
ymouse91 <35890980+ymouse91@users.noreply.github.com>
2025-06-02 10:46:48 +0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,14 @@ const FILES_TO_CACHE = [
   '/manifest.json',
   '/logo-192.png',
   '/logo-512.png',
-  '/favicon.ico'
+  '/favicon.ico',
+  '/static/css/main.d8f800cf.css',
+  '/static/css/main.d8f800cf.css.map',
+  '/static/js/453.6336793f.chunk.js',
+  '/static/js/453.6336793f.chunk.js.map',
+  '/static/js/main.2a2c43d9.js',
+  '/static/js/main.2a2c43d9.js.LICENSE.txt',
+  '/static/js/main.2a2c43d9.js.map'
 ];
 
 // Asennusvaihe – cachetetaan resurssit
